50e40a7d57418c67ff352ca13cac5ac85c6fcfb3
justinadr <justinuminga@gmail.com>
2026-03-04 01:21:10 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Minimal real-world vulnerable web examples
---
+++ sql_injection/index.php
@@ -0,0 +1,34 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'vuln_db';
+
+$connect = mysqli_connect($host, $user, $password, $database);
+
+if (isset($_GET['id'])) {
+    
+    $id = $_GET['id'];
+    
+    // $id is wrapped in single quotes but not "escaped"
+    $query = "SELECT username, email FROM users WHERE id = '$id'";
+    
+    echo "<b>Executing Query:</b> " . $query . "<br><hr>";
+
+    $result = mysqli_query($connect, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "Username: " . $row["username"] . " - Email: " . $row["email"] . "<br>";
+        }
+    } else {
+        echo "0 results found.";
+    }
+} else {
+    echo "Please provide an ID. Example: index.php?id=1";
+}
+
+mysqli_close($connect);
+
+?>
